ee57b908a0acec911a5d3c527582844f96930da0
unknown <lopatin601@gmail.com>
2019-10-17 23:56:55 +0300

    section 6

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -1,1 +1,14 @@
-console.log("sfdfsdfsf");
+import React from "react";
+import ReactDOM from "react-dom";
+import { Router } from "react-router-dom";
+import { createBrowserHistory } from "history";
+import Routes from "./Routes";
+
+const history = createBrowserHistory();
+
+ReactDOM.hydrate(
+  <Router history={history}>
+    <Routes />
+  </Router>,
+  document.getElementById("root")
+);
